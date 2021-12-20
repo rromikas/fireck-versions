88acc6cf0ce262ce2115ab5a6093344970dcbd6d
rromikas <rromikas@gmail.com>
2021-12-20 04:01:36 +0200

    update

---
+++ build/static/js/19.970416c8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfireck-cms"]=this["webpackJsonpfireck-cms"]||[]).push([[19],{1035:function(c,t,e){"use strict";e.r(t);var n=e(0),s=e.n(n),a=e(86),i=e(30),o=e(34),u=e(2);t.default=function(){var c=Object(o.k)();return s.a.useEffect((function(){var t=window.location.hash;t&&Object(a.e)(t,(function(t){i.a.dispatch({type:"SET_NETLIFY_ACCESS_TOKEN",payload:t}),c.push("/")}))}),[c]),Object(u.jsx)("div",{})}}}]);
+//# sourceMappingURL=19.970416c8.chunk.js.map
